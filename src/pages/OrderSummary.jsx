a7ffe3f95328658734fc8bc0ec73731338862b1b
Shakeeb <shakeebparavannur@gmail.com>
2023-07-31 19:12:04 +0530

    fix:payment bug

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -1,33 +1,63 @@
-import React from 'react'
+import React from "react";
 
-const OrderSummary = ({bookingData}) => {
-    console.log(bookingData);
+const OrderSummary = ({ bookingData }) => {
+  console.log(bookingData);
   return (
     <div>
-        {""}
-        <h3 className='text-success'>Booking Summary</h3>
-        <div className="mt-3">
-            <div className="border py-3 px-2"> Username:{bookingData.userId} </div>
-            <div className="border py-3 px-2">Date:{bookingData.dateOfTrip}</div>
-            <div className="border py-3 px-2">No of Person:{bookingData.noOfPerson}</div>
-            <div className="border py-3 px-2">Price per head{bookingData.noOfPerson}</div>
-            <div className="border py-3 px-2">
-                <h4 className='text-success'>Menu Item</h4>
-                <div className="p-3">
-                    <div className="d-flex">
-                        <div className='d-flex w-100 justify-content-between'>
-                            <p>Menu Item Name</p>
-                            <p>$10 x 10 = </p>
-                        </div>
-                        <p style={{width:"70px",textAlign:"right"}}>$100</p>
-                    </div>
-                    <hr />
-                    <h4 className="text-danger" style={{textAlign:"right"}}>$100</h4>
-                </div>
-            </div>
+      {""}
+      <h3 className="text-success">Booking Summary</h3>
+      <div className="mt-3">
+        <div className="border d-flex justify-content-between py-3 px-2">
+          <span className="fw-bold">Username:</span>
+          <span className="ps-">{bookingData.userId}</span>{" "}
         </div>
-    </div>
-  )
-}
+        <div className="border d-flex justify-content-between py-3 px-2">
+          <span className="fw-bold">Place:</span>
+          <span className="ps-">{bookingData.package.location}</span>{" "}
+        </div>
+        <div className="border d-flex justify-content-between py-3 py-3 px-2">
+          <span className="fw-bold"> Date:</span>
+          {new Date(bookingData.dateOfTrip).toLocaleDateString()}
+        </div>
+        <div className="border d-flex justify-content-between py-3 py-3 px-2">
+          <span className="fw-bold">No of Person:</span>
+          {bookingData.noOfPerson}
+        </div>
+        <div className="border d-flex justify-content-between py-3 py-3 px-2">
+          <span className="fw-bold">Price per head</span>
+          {bookingData.package.pricePerHead}
+        </div>
 
-export default OrderSummary
+        <div className="border d-flex justify-content-between py-3 py-3 px-2">
+          <span></span>
+          <h4 className="text-success mt-5">Total Amount</h4>
+          <div className="p-3">
+            <div className="d-flex">
+              <div className="d-flex w-100 justify-content-between">
+                <p></p>
+                <p>
+                  <span style={{ fontFamily: " 'Montserrat', sans-serif" }}>
+                    ₹
+                  </span>
+                  {bookingData.package.pricePerHead} x{" "}
+                  <span style={{ fontFamily: " 'Montserrat', sans-serif" }}>
+                    ₹
+                  </span>
+                  {bookingData.noOfPerson}
+                </p>
+              </div>
+              {/* <p style={{ width: "70px", textAlign: "right" }}>$100</p> */}
+            </div>
+            <hr />
+            <h4 className="text-danger" style={{ textAlign: "right" }}>
+              <span style={{ fontFamily: " 'Montserrat', sans-serif" }}>₹</span>
+              {bookingData.amount}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
